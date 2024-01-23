8a4139bc8b9177e3539d75642e4a08ea517560a6
HiranSasitha <hiransasitha123@gamil.com>
2024-01-23 23:03:32 +0530

    completed orderdev

--- src/main/java/com/ijse/bookShop/entity/ItemEntity.java
+++ src/main/java/com/ijse/bookShop/entity/ItemEntity.java
@@ -1,14 +1,22 @@
 package com.ijse.bookShop.entity;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -51,6 +59,11 @@ public class ItemEntity {
     @JoinColumn(name = "itemcat_id",nullable = false)
 
     private ItemCategoryEntity itemCategoryEntity;
+
+    @OneToMany(fetch = FetchType.LAZY,mappedBy = "itemEntity",targetEntity = OrderItemsEntity.class,cascade = CascadeType.ALL)
+    private Set<OrderItemsEntity> orderItemsEntities = new HashSet<>();
+    
+    
 
 
     
--- src/main/java/com/ijse/bookShop/entity/UserEntity.java
+++ src/main/java/com/ijse/bookShop/entity/UserEntity.java
@@ -1,14 +1,22 @@
 package com.ijse.bookShop.entity;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.hibernate.annotations.ManyToAny;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -39,6 +47,10 @@ public class UserEntity {
     @ManyToOne
     @JoinColumn(name = "category_id",nullable = false)
     private UserCategoryEntity userCategoryEntity;
+
+    @JsonIgnore
+    @OneToMany(fetch = FetchType.LAZY,mappedBy = "userEntity",targetEntity = OrderEntity.class,cascade = CascadeType.ALL)
+    private Set<OrderEntity>orderEntities = new HashSet<>();
 
 
     
